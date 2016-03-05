4673561177b62f55e08d97cfca48aed9fb42976e
Calvin Sauer <calvin.j.sauer@gmail.com>
2016-03-05 09:52:51 -0600

    Mocha, istanbul, coveralls, and travis-ci

---
+++ test/modules/app.test.js
@@ -0,0 +1,9 @@
+import App from '../../src/modules/App.js';
+import chai from 'chai';
+const expect = chai.expect;
+
+describe("App", () => {
+  it("should exist", () => {
+    expect(App).to.be.ok;
+  });
+});
