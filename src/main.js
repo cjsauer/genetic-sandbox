f48cd42883319e92d8720fc8e6211122428d95cf
Calvin Sauer <calvin.j.sauer@gmail.com>
2016-04-21 07:34:15 -0500

    Fullscreen canvas and nice background color

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@
 // it becomes the "homepage"
 import "file?name=index.html!./main.html";
 
-// Load the main stylesheet
+// Load the reset stylesheet to get consistent styling across browsers
 import "./styles/reset.css";
+
+// Load the GS theme
+import "./styles/theme.css";
 
 // Import the Paper.js vector grahics library
 import paper from "paper";
@@ -13,6 +16,17 @@ import App from "./modules/App";
 
 // Export the GS bootstrapping function
 window.GeneticSandbox = function(canvas) {
+  function fitToContainer() {
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+  }
+
+  // Resize the canvas to fill its containing element
+  window.onresize = fitToContainer;
+  fitToContainer();
+
   // Add systems to this list to include them in the processing loop
   let systems = [
   ];
