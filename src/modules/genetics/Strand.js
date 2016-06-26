49e32f14a11a5267082e224bac306f8481962a43
Calvin Sauer <calvin.j.sauer@gmail.com>
2016-06-26 13:36:40 -0500

    Implement NEAT functionality in Strand class

--- src/modules/genetics/Strand.js
+++ src/modules/genetics/Strand.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import Serializable from "../util/Serializable";
 import NodeGene from "./NodeGene";
 import ConnectionGene from "./ConnectionGene";
@@ -29,8 +30,6 @@ class Strand extends Serializable {
   constructor(inputCount, outputCount, enabled, random) {
     super("strand");
 
-    if (arguments.length === 0) return;
-
     /**
      * The list of node genes describing neurons
      * @type {NodeGene[]}
@@ -49,6 +48,8 @@ class Strand extends Serializable {
      * @type {number}
      */
     this._nextNodeGeneID = 1;
+
+    if (arguments.length === 0) return;
 
     let inputNodeGenes = [];
     let outputNodeGenes = [];
@@ -102,6 +103,245 @@ class Strand extends Serializable {
   get hiddenNodeGeneCount() {
     const isHiddenNodeGene = (gene) => gene.type === "hidden";
     return this.nodeGenes.filter(isHiddenNodeGene).length;
+  }
+
+  /**
+   * Randomly perturbs this strand's connection gene weights, possibly
+   * replacing the weight with a new value completely. Chances are NOT
+   * codependent.
+   * @param {number} perturbChance - chance that each weight has of being mutated
+   * @param {number} perturnAmplitude - maximum change in weight possible
+   * @param {number} newValueChance - chance that a weight will be completely
+   * replaced by a new value
+   * @param {Object} random-js generator instance
+   */
+  mutateWeights(perturbChance, perturbAmplitude, newValueChance, random) {
+    this.connectionGenes.forEach((gene) => {
+      if (random.bool(perturbChance)) {
+        let delta = perturbAmplitude * random.real() * (random.bool(0.5) ? 1 : -1);
+        gene.weight += delta;
+      }
+
+      if (random.bool(newValueChance)) {
+        gene.weight = random.real();
+      }
+    });
+  }
+
+  /**
+   * Splits the given connection gene with a new node gene. The original
+   * connection is disabled and two new connection genes are added to the
+   * strand. The new connection leading into the new node receives a weight of
+   * 1, and the new connection leading out of the new node receives the same
+   * weight as the original connection.
+   * @param {ConnectionGene} connectionGene - connection gene to split
+   */
+  _splitConnectionWithNode(connectionGene) {
+    const nodeGene = new NodeGene(this._nextNodeGeneID++, "hidden");
+    const incomingConnection = new ConnectionGene(connectionGene.in, nodeGene.id, 1, true);
+    const outgoingConnection = new ConnectionGene(nodeGene.id, connectionGene.out, connectionGene.weight, true);
+    connectionGene.enabled = false;
+    this.nodeGenes.push(nodeGene);
+    this.connectionGenes.push(incomingConnection);
+    this.connectionGenes.push(outgoingConnection);
+  }
+
+  /**
+   * Chooses a connection gene at random and splits it with a new node gene
+   * @param {Object} random - random-js generator instance
+   */
+  addRandomNodeGene(random) {
+    const connectionToSplit = random.pick(this.connectionGenes);
+    this._splitConnectionWithNode(connectionToSplit);
+  }
+
+  /**
+   * Determines if two node genes are connected
+   * @private
+   * @param {NodeGene} sourceNode - source node of the potential connection
+   * @param {NodeGene} destNode - destination node of the potential connection
+   * @returns {boolean} true if the two node genes are already connected, false
+   * otherwise
+   */
+  _areConnected(sourceNode, destNode) {
+    return this.connectionGenes.some((gene) => {
+      return (gene.in === sourceNode.id && gene.out === destNode.id);
+    });
+  }
+
+  /**
+   * Determines if two node genes are capable of being connected
+   * @private
+   * @param {NodeGene} sourceNode - the source node of the potential connection
+   * @param {NodeGene} destNode - the destination node of the potential
+   * connection
+   * @returns {boolean} true if the given node genes are able to connect, false
+   * otherwise
+   */
+  _canConnect(sourceNode, destNode) {
+    return sourceNode.type !== "output" &&
+           destNode.type !== "input" &&
+           !this._areConnected(sourceNode, destNode);
+  }
+
+  /**
+   * Connects the two given node genes with a new connection gene
+   * @private
+   * @param {NodeGene} sourceNode - the source node of the new connection
+   * @param {NodeGene} destNode - the destination node of the new connection
+   * @param {ConnectionGene} the new connection gene, or null if that connection
+   * is not valid
+   */
+  _connect(sourceNode, destNode, random) {
+    let connectionGene = null;
+    if (this._canConnect(sourceNode, destNode)) {
+      connectionGene = new ConnectionGene(sourceNode.id, destNode.id, random.real(), true);
+      this.connectionGenes.push(connectionGene);
+    }
+    return connectionGene;
+  }
+
+  /**
+   * Attempts to add a random connection gene to the genome, failing after
+   * the given number of iterations
+   * @param {number} iterations - number of times to randomly try selecting two
+   * node genes to connect
+   * @param {Object} random - random-js generator instance
+   * @returns {ConnectionGene} the added connection gene, or null if no gene could
+   * be added
+   */
+  addRandomConnectionGene(iterations, random) {
+    let connectionGene = null;
+    for (let attempts = 0; attempts < iterations; attempts++) {
+      // Randomly select two nodes...
+      let sourceNode = random.pick(this.nodeGenes);
+      let destNode = random.pick(this.nodeGenes);
+
+      // ...and attempt to connect them
+      connectionGene = this._connect(sourceNode, destNode, random);
+      if (connectionGene !== null) break;
+    }
+    return connectionGene;
+  }
+
+  /**
+   * Returns the matching connection genes between this and the given strand.
+   * Connection genes are considered matching if they have the same innovation
+   * number.
+   * @private
+   * @param {Strand} otherStrand - the strand to compare this one against
+   * @returns {ConnectionGene[]} array of matching connection genes
+   */
+  _matching(otherStrand) {
+    return _.intersectionBy(this.connectionGenes, otherStrand.connectionGenes, (gene) => {
+      return gene.innovationNumber;
+    });
+  }
+
+  /**
+   * Returns the disjoint connection genes between this and the given strand.
+   * Connection genes are considered disjoint if they are not matching in the
+   * middle of the strands.
+   * @private
+   * @param {Strand} otherStrand - the strand to compare this one against
+   * @returns {ConnectionGene[]} array of disjoint connection genes
+   */
+  _disjoint(otherStrand) {
+    const result = [];
+    // Only consider non-matching genes
+    let matchingCount = this._matching(otherStrand).length;
+    let candidates = _.drop(this.connectionGenes, matchingCount);
+    let others = _.drop(otherStrand.connectionGenes, matchingCount);
+
+    // Test whether the given candidate has an innovation number that's less
+    // than any gene's innovation number in the other strand
+    candidates.forEach((candidate) => {
+      if (others.some((gene) => { return gene.innovationNumber > candidate.innovationNumber; })) {
+        result.push(candidate);
+      }
+    });
+    return result;
+  }
+
+  /**
+   * Returns the excess connection genes between this and the given strand.
+   * Connection genes are considered excess if they are not matching at the
+   * end of the strand.
+   * @private
+   * @param {Strand} otherStrand - the strand to compare this one against
+   * @returns {ConnectionGene[]} array of excess connection genes
+   */
+  _excess(otherStrand) {
+    const result = [];
+    // Only consider non-matching genes
+    let matchingCount = this._matching(otherStrand).length;
+    let candidates = _.drop(this.connectionGenes, matchingCount);
+    let others = _.drop(otherStrand.connectionGenes, matchingCount);
+
+    // Test whether the given candidate has an innovation number that's greater
+    // than every gene's innovation number in the other strand
+    candidates.forEach((candidate) => {
+      if (others.every((gene) => { return gene.innovationNumber < candidate.innovationNumber; })) {
+        result.push(candidate);
+      }
+    });
+    return result;
+  }
+
+  /**
+   * Computes the compatibility difference between two strands
+   * @param {Strand} otherStrand - the strand to compare this one against
+   * @param {number} excessCoefficient - the importance placed on the number
+   * of excess genes in determining compatibility
+   * @param {number} disjointCoefficient - the importance placed on the number
+   * of disjoint genes in determining compatibility
+   * @param {number} weightCoefficient - the importance placed on differences
+   * in weights between matching connections in determining compatibility
+   * @returns {number} the computed compatibility distance measure
+   */
+  compatibilityDistance(otherStrand, excessCoefficient, disjointCoefficient, weightCoefficient) {
+    // Determine the number of genes in the larger genome, normalized to 1 if
+    // both genomes are "small" (less than 20 genes)
+    let myLength = this.connectionGenes.length;
+    let otherLength = otherStrand.connectionGenes.length;
+    let N = myLength >= otherLength ? myLength : otherLength;
+    if (N < 20) N = 1;
+
+    // Compute matching, disjoint, and excess gene counts
+    let myMatchingGenes = this._matching(otherStrand);
+    let otherMatchingGenes = otherStrand._matching(this);
+    let myDisjoints = this._disjoint(otherStrand);
+    let otherDisjoints = otherStrand._disjoint(this);
+    let totalDisjoints = myDisjoints.length + otherDisjoints.length;
+    let myExcess = this._excess(otherStrand);
+    let otherExcess = otherStrand._excess(this);
+    let totalExcess = myExcess.length + otherExcess.length;
+
+    // Compute the average weight difference between matching genes
+    let totalWeightDifference = 0;
+    for (let i = 0; i < myMatchingGenes.length; i++) {
+      totalWeightDifference += Math.abs(myMatchingGenes[i].weight - otherMatchingGenes[i].weight);
+    }
+    let averageWeightDifference = totalWeightDifference / myMatchingGenes.length;
+
+    // Compute the final compatibility distance
+    return (excessCoefficient * totalExcess / N) + (disjointCoefficient * totalDisjoints / N) + (weightCoefficient * averageWeightDifference);
+  }
+
+  /**
+   * Makes a complete copy of this strand
+   * @returns {Strand} the cloned strand
+   */
+  clone() {
+    let strand = new Strand();
+    strand._nextNodeGeneID = this._nextNodeGeneID;
+    this.nodeGenes.forEach((gene) => {
+      strand.nodeGenes.push(new NodeGene(gene.id, gene.type));
+    });
+    this.connectionGenes.forEach((gene) => {
+      strand.connectionGenes.push(new ConnectionGene(gene.in, gene.out, gene.weight, gene.enabled));
+    });
+    return strand;
   }
 }
 
--- src/modules/genetics/strand.test.js
+++ src/modules/genetics/strand.test.js
@@ -1,7 +1,8 @@
 import Strand from "./Strand";
+import ConnectionGene from "./ConnectionGene";
 import Serializable from "../util/Serializable";
 import { expect } from "chai";
-import { stub } from "sinon";
+import { stub, spy } from "sinon";
 
 describe("Strand", () => {
   let random;
@@ -12,6 +13,10 @@ describe("Strand", () => {
     random = {
       real: stub().returns(0.5)
     };
+
+    // Completely reset innovations each test for the sake of ease
+    ConnectionGene.resetInnovations();
+    ConnectionGene._nextInnovationNumber = 1;
   });
 
   it("should extend Serializable", () => {
@@ -84,5 +89,231 @@ describe("Strand", () => {
     expect(strand.inputNodeGeneCount).to.equal(2);
     expect(strand.outputNodeGeneCount).to.equal(3);
     expect(strand.hiddenNodeGeneCount).to.equal(0);
+  });
+
+  it("can determine if two node genes are already connected", () => {
+    let strand = new Strand(2, 1, true, random);
+    strand._splitConnectionWithNode(strand.connectionGenes[0]);
+    let nodeGenes = strand.nodeGenes;
+
+    expect(strand._areConnected(nodeGenes[0], nodeGenes[3])).to.be.true;
+    expect(strand._areConnected(nodeGenes[3], nodeGenes[2])).to.be.true;
+    expect(strand._areConnected(nodeGenes[1], nodeGenes[2])).to.be.true;
+    expect(strand._areConnected(nodeGenes[0], nodeGenes[1])).to.be.false;
+    expect(strand._areConnected(nodeGenes[1], nodeGenes[3])).to.be.false;
+    expect(strand._areConnected(nodeGenes[3], nodeGenes[1])).to.be.false;
+    expect(strand._areConnected(nodeGenes[2], nodeGenes[1])).to.be.false;
+  });
+
+  it("can determine if two node genes could potentially be connected", () => {
+    let strand = new Strand(2, 1, true, random);
+    strand._splitConnectionWithNode(strand.connectionGenes[0]);
+
+    // A valid connection
+    expect(strand._canConnect(strand.nodeGenes[1], strand.nodeGenes[3])).to.be.true;
+
+    // Cannot use an input node as a destination
+    expect(strand._canConnect(strand.nodeGenes[1], strand.nodeGenes[0])).to.be.false;
+    expect(strand._canConnect(strand.nodeGenes[0], strand.nodeGenes[1])).to.be.false;
+
+    // Cannot use an output node as a source
+    expect(strand._canConnect(strand.nodeGenes[2], strand.nodeGenes[3])).to.be.false;
+
+    // Cannot connect two nodes that are already connected
+    expect(strand._canConnect(strand.nodeGenes[0], strand.nodeGenes[3])).to.be.false;
+    expect(strand._canConnect(strand.nodeGenes[1], strand.nodeGenes[2])).to.be.false;
+    expect(strand._canConnect(strand.nodeGenes[3], strand.nodeGenes[2])).to.be.false;
+  });
+
+  it("can be cloned", () => {
+    let strand = new Strand(2, 1, true, random);
+    let clone = strand.clone();
+    expect(strand.connectionGenes).to.eql(clone.connectionGenes);
+    expect(strand.nodeGenes).to.eql(clone.nodeGenes);
+    expect(strand._nextNodeGeneID).to.equal(clone._nextNodeGeneID);
+  });
+
+  describe("mutation", () => {
+    it("can randomly change its connection weights", () => {
+      let strand = new Strand(2, 1, true, random);
+
+      // Force the random chance of each mutation to alternate
+      random.real.returns(0.1);
+      random.bool = stub();
+      random.bool.withArgs(0.1)
+      .onFirstCall().returns(true) // Will perturb first weight...
+      .onSecondCall().returns(false); // but not second
+      random.bool.withArgs(0.2)
+      .onFirstCall().returns(false) // Will NOT replace first weight...
+      .onSecondCall().returns(true); // but will replace second
+      random.bool.returns(false);
+
+      strand.mutateWeights(0.1, 0.05, 0.2, random);
+
+      let weights = strand.connectionGenes.map((gene) => gene.weight);
+      expect(weights[0]).to.equal(0.495);
+      expect(weights[1]).to.equal(0.1);
+    });
+
+    it("can split a given connection gene with a node gene, preserving behavior", () => {
+      let strand = new Strand(2, 1, true, random);
+      let originalConnectionGeneCount = strand.connectionGenes.length;
+      let connectionGene = strand.connectionGenes[0];
+
+      strand._splitConnectionWithNode(connectionGene);
+
+      let incomingConnection = strand.connectionGenes[originalConnectionGeneCount];
+      let outgoingConnection = strand.connectionGenes[originalConnectionGeneCount + 1];
+      expect(connectionGene.enabled).to.be.false;
+      expect(strand.connectionGenes.length).to.equal(originalConnectionGeneCount + 2);
+      expect(incomingConnection.weight).to.equal(1);
+      expect(outgoingConnection.weight).to.equal(connectionGene.weight);
+    });
+
+    it("can split a random connection with a node gene, preserving behavior", () => {
+      let strand = new Strand(2, 1, true, random);
+      let originalNodeGeneCount = strand.nodeGenes.length;
+      spy(strand, "_splitConnectionWithNode");
+      random.pick = stub().returns(strand.connectionGenes[0]);
+
+      strand.addRandomNodeGene(random);
+      expect(strand.nodeGenes).to.have.lengthOf(originalNodeGeneCount + 1);
+      expect(strand._splitConnectionWithNode.calledWith(strand.connectionGenes[0])).to.be.true;
+    });
+
+    it("can connect two given nodes", () => {
+      let strand = new Strand(1, 2, true, random);
+      strand._splitConnectionWithNode(strand.connectionGenes[0]);
+      let originalConnectionGeneCount = strand.connectionGenes.length;
+      let nodeGenes = strand.nodeGenes;
+
+      // Valid connection
+      let connection = strand._connect(nodeGenes[3], nodeGenes[2], random);
+      expect(strand.connectionGenes).to.have.lengthOf(originalConnectionGeneCount += 1);
+      expect(connection.weight).to.equal(0.5);
+
+      // Recurrent connection
+      connection = strand._connect(nodeGenes[3], nodeGenes[3], random);
+      expect(strand.connectionGenes).to.have.lengthOf(originalConnectionGeneCount += 1);
+      expect(connection.weight).to.equal(0.5);
+
+      // Invalid connection
+      connection = strand._connect(nodeGenes[0], nodeGenes[3], random);
+      expect(strand.connectionGenes).to.have.lengthOf(originalConnectionGeneCount);
+      expect(connection).to.be.null;
+    });
+
+    it("can randomly add a new connection between two nodes", () => {
+      let strand = new Strand(1, 2, true, random);
+      strand._splitConnectionWithNode(strand.connectionGenes[0]);
+      let originalConnectionGeneCount = strand.connectionGenes.length;
+      random.pick = stub();
+
+      // Test a normal connection
+      random.pick.onCall(0).returns(strand.nodeGenes[3]);
+      random.pick.onCall(1).returns(strand.nodeGenes[2]);
+
+      // Test a recurrent connection
+      random.pick.onCall(2).returns(strand.nodeGenes[3]);
+      random.pick.onCall(3).returns(strand.nodeGenes[3]);
+
+      // Test the event of two appropriate nodes never being found
+      random.pick.returns(strand.nodeGenes[0]);
+
+      let connection = strand.addRandomConnectionGene(3, random);
+      expect(strand.connectionGenes).to.have.lengthOf(originalConnectionGeneCount += 1);
+      expect(connection.weight).to.equal(0.5);
+
+      connection = strand.addRandomConnectionGene(3, random);
+      expect(strand.connectionGenes).to.have.lengthOf(originalConnectionGeneCount += 1);
+      expect(connection.weight).to.equal(0.5);
+
+      connection = strand.addRandomConnectionGene(3, random);
+      expect(strand.connectionGenes).to.have.lengthOf(originalConnectionGeneCount);
+      expect(connection).to.be.null;
+    });
+  });
+
+  describe("speciation", () => {
+    let strand1, strand2;
+
+    beforeEach(() => {
+      // Mimic some evolution of two strands using the two examples given on
+      // page 12 of the NEAT pdf: http://nn.cs.utexas.edu/downloads/papers/stanley.ec02.pdf
+      strand1 = new Strand(3, 1, true, random);
+      strand1._splitConnectionWithNode(strand1.connectionGenes[1]);
+      strand2 = new Strand(3, 1, true, random);
+      strand2._splitConnectionWithNode(strand2.connectionGenes[1]);
+      strand2._splitConnectionWithNode(strand2.connectionGenes[4]);
+      strand1._connect(strand1.nodeGenes[0], strand1.nodeGenes[4], random);
+      strand2._connect(strand2.nodeGenes[2], strand2.nodeGenes[4], random);
+      strand2._connect(strand2.nodeGenes[0], strand2.nodeGenes[5], random);
+    });
+
+    it("can determine matching genes between strands", () => {
+      let matchingGenes = strand1._matching(strand2);
+      expect(matchingGenes).to.have.lengthOf(5);
+      expect(matchingGenes[0].innovationNumber).to.equal(1);
+      expect(matchingGenes[1].innovationNumber).to.equal(2);
+      expect(matchingGenes[2].innovationNumber).to.equal(3);
+      expect(matchingGenes[3].innovationNumber).to.equal(4);
+      expect(matchingGenes[4].innovationNumber).to.equal(5);
+
+      matchingGenes = strand2._matching(strand1);
+      expect(matchingGenes).to.have.lengthOf(5);
+      expect(matchingGenes[0].innovationNumber).to.equal(1);
+      expect(matchingGenes[1].innovationNumber).to.equal(2);
+      expect(matchingGenes[2].innovationNumber).to.equal(3);
+      expect(matchingGenes[3].innovationNumber).to.equal(4);
+      expect(matchingGenes[4].innovationNumber).to.equal(5);
+    });
+
+    it("can determine disjoint genes between strands", () => {
+      let disjointGenes = strand1._disjoint(strand2);
+      expect(disjointGenes).to.have.lengthOf(1);
+      expect(disjointGenes[0].innovationNumber).to.equal(8);
+
+      disjointGenes = strand2._disjoint(strand1);
+      expect(disjointGenes).to.have.lengthOf(2);
+      expect(disjointGenes[0].innovationNumber).to.equal(6);
+      expect(disjointGenes[1].innovationNumber).to.equal(7);
+    });
+
+    it("can determine excess genes between strands", () => {
+      let excessGenes = strand1._excess(strand2);
+      expect(excessGenes).to.have.lengthOf(0);
+
+      excessGenes = strand2._excess(strand1);
+      expect(excessGenes).to.have.lengthOf(2);
+      expect(excessGenes[0].innovationNumber).to.equal(9);
+      expect(excessGenes[1].innovationNumber).to.equal(10);
+    });
+
+    it("can compute the compatibility difference between two strands", () => {
+      let distance = strand1.compatibilityDistance(strand2, 1.0, 1.0, 1.0);
+      expect(distance).to.equal(5);
+      distance = strand1.compatibilityDistance(strand2, 0.5, 1.0, 1.0);
+      expect(distance).to.equal(4);
+      distance = strand1.compatibilityDistance(strand2, 0.5, 0.5, 1.0);
+      expect(distance).to.equal(2.5);
+
+      // Moving weights farther apart should increase the compatibility distance
+      strand1.connectionGenes[0].weight = 0.2;
+      strand1.connectionGenes[1].weight = 0.1;
+      strand1.connectionGenes[2].weight = 0.9;
+      let mutatedDistance = strand1.compatibilityDistance(strand2, 0.5, 0.5, 1.0);
+      expect(mutatedDistance).to.be.above(distance);
+
+      // Evolving the first strand to closer match the second should decrease
+      // its compatibility distance
+      strand1._splitConnectionWithNode(strand1.connectionGenes[4]);
+      let evolvedDistance = strand1.compatibilityDistance(strand2, 0.5, 0.5, 1.0);
+      expect(evolvedDistance).to.be.below(distance);
+
+      // There is zero distance between a strand and itself
+      let clone = strand1.clone();
+      distance = strand1.compatibilityDistance(clone, 1.0, 1.0, 1.0);
+      expect(distance).to.equal(0);
+    });
   });
 });
